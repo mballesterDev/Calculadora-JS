63c31fd6feaf5ddf9ade29bf107e5306d9ecb83a
mballesterDev <mballesterdev@gmail.com>
2024-01-18 20:48:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const pantalla = document.querySelector('#pantalla');
+const botones = document.querySelectorAll('.btn');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', function () {
+        const botonApretado = boton.textContent;
+
+        if (boton.id === 'c') {
+            pantalla.textContent = '0';
+            return;
+        }
+
+        if (boton.id === 'borrar') {
+            pantalla.textContent = pantalla.textContent.slice(0, -1);
+            return;
+        }
+
+        if (boton.id === 'igual') {
+            pantalla.textContent = eval(pantalla.textContent);
+            return;
+        }
+
+
+
+        if (pantalla.textContent === "0") {
+            pantalla.textContent = botonApretado;
+        } else {
+            pantalla.textContent += botonApretado;
+        }
+
+
+
+    });
+});
